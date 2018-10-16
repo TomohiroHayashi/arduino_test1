3d7d514fa37d2db2bd3dc2dcc57729973abe1f64
Tomohiro Hayashi <hayashi@vivita.co>
2018-10-16 14:37:46 +0900

    Change Wire.h to empty almost

--- core_lib/Wire.h
+++ core_lib/Wire.h
@@ -11,16 +11,16 @@
 class TwoWire : public Stream
 {
   private:
-    static uint8_t rxBuffer[];
-    static uint8_t rxBufferIndex;
-    static uint8_t rxBufferLength;
+    //static uint8_t rxBuffer[];
+    //static uint8_t rxBufferIndex;
+    //static uint8_t rxBufferLength;
 
-    static uint8_t txAddress;
-    static uint8_t txBuffer[];
-    static uint8_t txBufferIndex;
-    static uint8_t txBufferLength;
+    //static uint8_t txAddress;
+    //static uint8_t txBuffer[];
+    //static uint8_t txBufferIndex;
+    //static uint8_t txBufferLength;
 
-    static uint8_t transmitting;
+    //static uint8_t transmitting;
     //static void (*user_onRequest)(void);
     //static void (*user_onReceive)(int);
     //static void onRequestService(void);
@@ -28,11 +28,10 @@ class TwoWire : public Stream
   public:
     TwoWire();
     void begin() {
-      rxBufferIndex = 0;
-      rxBufferLength = 0;
-
-      txBufferIndex = 0;
-      txBufferLength = 0;
+      //rxBufferIndex = 0;
+      //rxBufferLength = 0;
+      //txBufferIndex = 0;
+      //txBufferLength = 0;
 
       //twi_init();
       //twi_attachSlaveTxEvent(onRequestService); // default callback must exist
@@ -45,22 +44,22 @@ class TwoWire : public Stream
     //void setClock(uint32_t);
     void beginTransmission(uint8_t address) {
       // indicate that we are transmitting
-      transmitting = 1;
+      //transmitting = 1;
       // set address of targeted slave
-      txAddress = address;
+      //txAddress = address;
       // reset tx buffer iterator vars
-      txBufferIndex = 0;
-      txBufferLength = 0;
+      //txBufferIndex = 0;
+      //txBufferLength = 0;
     }
     void beginTransmission(int address) { beginTransmission((uint8_t)address); }
     uint8_t endTransmission(void) { return endTransmission(true); }
     uint8_t endTransmission(uint8_t sendStop) {
       uint8_t ret = 0;
       // reset tx buffer iterator vars
-      txBufferIndex = 0;
-      txBufferLength = 0;
+      //txBufferIndex = 0;
+      //txBufferLength = 0;
       // indicate that we are done transmitting
-      transmitting = 0;
+      //transmitting = 0;
       return ret;
     }
     uint8_t requestFrom(uint8_t address, uint8_t quantity) {
@@ -77,8 +76,8 @@ class TwoWire : public Stream
           isize = 3;
         }
         // write internal register address - most significant byte first
-        while (isize-- > 0)
-          write((uint8_t)(iaddress >> (isize*8)));
+        //while (isize-- > 0)
+        //  write((uint8_t)(iaddress >> (isize*8)));
         endTransmission(false);
       }
       // clamp to buffer length
@@ -87,8 +86,8 @@ class TwoWire : public Stream
       }
       uint8_t read = 0;
       // set rx buffer iterator vars
-      rxBufferIndex = 0;
-      rxBufferLength = read;
+      //rxBufferIndex = 0;
+      //rxBufferLength = read;
       return read;
     }
     uint8_t requestFrom(int address, int quantity) {
@@ -98,38 +97,39 @@ class TwoWire : public Stream
       return requestFrom((uint8_t)address, (uint8_t)quantity, (uint8_t)sendStop);
     }
     virtual size_t write(uint8_t data) {
-      if(transmitting){
+      //if(transmitting){
         // put byte in tx buffer
-        txBuffer[txBufferIndex] = data;
-        ++txBufferIndex;
+        //txBuffer[txBufferIndex] = data;
+        //++txBufferIndex;
         // update amount in buffer   
-        txBufferLength = txBufferIndex;
-      }else{
+        //txBufferLength = txBufferIndex;
+      //}else{
       // in slave send mode
         // reply to master
         //twi_transmit(&data, 1);
-      }
+      //}
       return 1;
     }
     virtual size_t write(const uint8_t *data, size_t quantity) {
-      if(transmitting){
+      //if(transmitting){
       // in master transmitter mode
-        for(size_t i = 0; i < quantity; ++i){
-          write(data[i]);
-        }
-      }
+      //  for(size_t i = 0; i < quantity; ++i){
+      //    write(data[i]);
+      //  }
+      //}
       return quantity;
     }
     virtual int available(void) {
-      return rxBufferLength - rxBufferIndex;
+      //return rxBufferLength - rxBufferIndex;
+      reuturn 1;
     }
     virtual int read(void) {
       int value = -1;      
       // get each successive byte on each call
-      if(rxBufferIndex < rxBufferLength){
-        value = rxBuffer[rxBufferIndex];
-        ++rxBufferIndex;
-      }
+      //if(rxBufferIndex < rxBufferLength){
+      //  value = rxBuffer[rxBufferIndex];
+      //  ++rxBufferIndex;
+      //}
       return value;
     }
     //virtual int peek(void);
